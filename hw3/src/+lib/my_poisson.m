f4ecc42e1acfa627e7ab7b97905517f59228b7b3
Tirth Wong <tirthwong@gmail.com>
2014-04-19 16:48:16 +0800

    mix mode

--- hw3/src/+lib/my_poisson.m
+++ hw3/src/+lib/my_poisson.m
@@ -1,17 +1,14 @@
-function F = poisson( foreground, background, mask )
+function F = my_poisson( foreground, background, mask )
 
-%background = imr(background_name);
-%foreground = imr(foreground_name);
-%mask = imr(mask_name);
 
 if nargin < 3
     error('invalid');
 end
+
 if ndims(mask)==3
     warning(' input mask is 3d image. truncate to r channel');
     mask=mask(:,:,1);
 end
-
 
 foreground=imcheck(foreground);
 background=imcheck(background);
@@ -34,28 +31,47 @@ function F = poisson_gray( background, foreground, mask )
 
 n = w*h;
 
-A = sparse(n,n);
-f = zeros(n,1); %% Uknown Vector
-fx = find(mask > 0);  %% Unknown Position
-bx = find(mask == 0);  %% Known Position
-q = zeros(n,1);
-q(fx) = 1;
-% Build Laplace Matrix
+%A = sparse(n,n);
 
+f = zeros(n,1);         %% Uknown Vector 
+fx = find(mask > 0);    %% Unknown Position (target region)
+bx = find(mask == 0);   %% Known Position (BK)
+
+q = zeros(n,1);  
+q(fx) = 1;              % target region 
+
+
+% Build Laplace Matrix
+% 
+% 4-connected element
+% 
 I = diag(sparse(q));
 A = -4*I;
 A = A+circshift(I,[0 h])+circshift(I,[0 -h]); % X Comp
 A = A+circshift(I,[0 1])+circshift(I,[0 -1]); % Y Comp
-A = A+speye(n)-I;
-b = zeros(n,1);
-b(bx) = background(bx);
+A = A+speye(n)-I; % setup constriant 
+b = zeros(n,1);   % 
+b(bx) = background(bx); % fill constriant with BK(im2)
 
 % Compute Laplace Foreground
 lapforeground = circshift(foreground,[1 0])+circshift(foreground,[-1 0]);
 lapforeground = lapforeground+circshift(foreground,[0 1])+circshift(foreground,[0 -1]);
 lapforeground = lapforeground-4*foreground;
-b(fx) = lapforeground(fx);
 
+% lapBK = circshift(background,[1 0])+circshift(background,[-1 0]);
+% lapBK = lapBK+circshift(background,[0 1])+circshift(background,[0 -1]);
+% lapBK = lapBK-4*background;
+% 
+% gFor = imgradient(foreground);
+% gBK = imgradient(background);
+% 
+% resFB = lapforeground;
+% ch_flag= abs(gFor) < abs(gBK) ; 
+% resFB( ch_flag ) = lapBK(ch_flag);
+
+resFB = lapforeground;
+
+b(fx) = resFB(fx);
 % Solve Linear System Ax = b
 x = A\b;
 F = reshape(x,[h w]);
--- hw3/src/main.m
+++ hw3/src/main.m
@@ -2,7 +2,6 @@
 %
 %
 
-
 root='../data';
 
 im1=imread(fullfile(root,'venus.png'));
@@ -10,14 +9,60 @@ im2=imread(fullfile(root,'monalisa.png'));
 im3=imread(fullfile(root,'mask.png'));
 
 
-im1 = double(im1)/255.0;
-im2 = double(im2)/255.0;
-im3 = double(im3)/255.0;
-
-
-%%
+im1=imread(fullfile(root,'2.jpg'));
+im2=imread(fullfile(root,'1.jpg'));
+im3=imread(fullfile(root,'mask_.jpg'));
 
 F=lib.poisson(im1,im2,im3);
 
-
 fimshowpair(im1,F);
+
+%% small test
+
+m=1:12;
+m=reshape(m,3,[])';
+h=3;
+disp(m)
+%circshift(m,[0,1])
+%circshift(m,[1,0])
+
+% n-th element specifies the shift 
+% amount for the n-th dimension
+% of array A.
+circshift(m,[0,2])
+
+
+
+%% 
+
+im1=imread(fullfile(root,'im1_32.png'));
+im2=imread(fullfile(root,'im2_32.png'));
+im3=imread(fullfile(root,'mask_32.png'));
+
+F=lib.my_poisson(im1,im2,im3);
+
+fimshowpair(im1,im2);
+fimshowpair(im3,F);
+
+
+
+%%
+ 
+F=lib.my_poisson(im1,im2,im3);
+
+fimshowpair(im1,im2);
+fimshowpair(im3,F);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
