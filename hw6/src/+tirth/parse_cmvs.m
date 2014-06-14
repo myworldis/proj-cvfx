13821328c055eb0b0f14c90912a69512cbf19c3a
Tirth Wong <tirthwong@gmail.com>
2014-06-14 17:09:18 +0800

    ..

--- hw6/src/+tirth/parse_cmvs.m
+++ hw6/src/+tirth/parse_cmvs.m
@@ -6,4 +6,8 @@
 %
 
 
+IMG_DIR =fullfile(gdef.dataroot,'video','0834','IMG');
+CAM_DIR =fullfile(gdef.dataroot,'video','0834','dense\v1fix.nvm.cmvs\00\txt');
+CAM_VIS_DIR =fullfile(gdef.dataroot,'video','0834','dense\v1fix.nvm.cmvs\00\visualize');
+
 fpath='';
